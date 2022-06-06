3b6f4d110235b6def696b56a77682714a8eaa6d9
ValentinJDT <vjeandot@yahoo.com>
2022-06-06 16:39:00 +0200

    update tabcompleter

--- src/main/java/fr/mrcoq/command/WWTabCompleter.java
+++ src/main/java/fr/mrcoq/command/WWTabCompleter.java
@@ -23,7 +23,6 @@ public class WWTabCompleter implements TabCompleter {
             subCommand.ifPresent(subCommands -> sub.add(subCommands.getArgs()[args.length-1]));
         }
 
-
         return sub;
     }
 }
